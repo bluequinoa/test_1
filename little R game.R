7a9b08058cfc2e9ab997a6207a308d488d9f88c5
blue_quinoa <114580068+hugoregiss@users.noreply.github.com>
2022-09-28 19:30:03 +0200

    Update little R game.R

--- little R game.R
+++ little R game.R
@@ -1,58 +1,68 @@
-lat <- runif(1)
-lon <- runif(1)
-lat <- round(lat,1)
-lon <- round(lon,1)
+#parameters:
+lat <- runif(1,min = 0, max = 10)
+lon <- runif(1,min = 0, max = 10)
+lat <- round(lat,0)
+lon <- round(lon,0)
+choice_game <- ""
+choice_lat <- ""
+choice_lon <- ""
+score <- 0
 
-choice_lat<- ""
-score_lat <- 0
-while(choice_lat!=lat){
-  choice_lat <- readline(prompt = "guess lat : ")
-  if(choice_lat == lat){
+#sinon dicitonnaire de plusieurs capitales avec les lat/lon à prendre random
+#recuperer les lat/lon du dictionnaire avec les index
+#garder le nom de la capitale dans une variable (utile?)
+
+
+#function guess lat
+function_guess_lat <- function(lat,choice_lat,score)
+  while(choice_lat!=lat){
+    choice_lat <- readline(prompt = "guess lat : ")
+    if(choice_lat == lat){
+      print("well done! ")
+      print("now guess lon")
+    print(paste("for now your score is ",score))
+    }else if (choice_lat > lat){
+      print("less")
+      score <- score + 1
+    }else{
+      print("more")
+      score <- score + 1
+    }
+  }
+
+
+#function guess lon
+function_guess_lon <- function(lon,choice_lon,score)
+while(choice_lon!=lon){
+  choice_lon <- readline(prompt = "guess lon : ")
+  if(choice_lon == lon){
+    print(paste("your total score is ",score))
     print("well done! ")
-    print("now guess lon")
-  }else if (choice_lat > lat){
+  }else if (choice_lon > lon){
     print("less")
-    score-lat <- score_lat + 1
+    score <- score + 1
   }else{
     print("more")
-    score_lat <- score_lat + 1
+    score <- score + 1
   }
 }
 
 
 
-
-choice_lon <- ""
-
-while(choice_lon!=lon){
-  cat("1 - play\n2 - High Score\nq - quit")
-  choice <- readline(prompt = "what is your choice? ")
-  if(choice == 1){
-    print("A nice game here")
-  }else if (choice == 2){
-    print(df_player)
-  }
-}
-
-cat("1 - play\n2 - High Score\nq - quit")
-
-
-choice_game <- ""
+#start the game
+player_name <- readline(prompt = "what is your name ?")
 cat("1 - start playing\n2 - quit")
 choice_game <- readline(prompt = "what is your choice ?")
 if(choice_game == 1){
-  print("here we go!")
+  print(paste("here we go! ",player_name))
   print("In this game, your goal is to guess the lat/lon of a bird equiped with a gps")
   print("the bird is not mooving for the moment")
-  
-  #do the game
-}else if (choice_game == 2){
-  print("ciao looser")}
+  function_guess_lat(lat,choice_lat,score)
+  function_guess_lon(lon,choice_lon,score)
+}else if(choice_game == 2){
+  print(paste("ciao ",player_name," le gros noob"))
+}
 
-
-
-#input avec le nom du joueur 
-# subset() pour foutre tout dans un data set
-
-
-
+#store the score
+storage_score <- c(name=player_name,score=score)
+print(storage_score)
